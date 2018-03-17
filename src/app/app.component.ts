8995898cdf5e96f0ffc89cc8de8412300697115f
Johanna Gomez <pao_gomez1@hotmail.com>
2018-03-16 21:36:24 -0500

    objects and arrays in app components ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,19 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <h1>Hey guys</h1>
-    <p>How are you doing</p>
+    <p>{{myObject.location}}</p>
+    <ul>
+      <li *ngFor="let arr of myArr">{{arr}}</li>
+    </ul>
   `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'my angular app';
+  myObject = {
+    gender: 'female',
+    age: 38,
+    location: 'USA'
+  };
+  myArr = ['him', 'hers', 'yours'];
 }
